934606a05a9376cc246783e0d3f0371a48996eb8
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-07-23 10:36:02 +0530

    Arrays

---
+++ Array/Main.java
@@ -0,0 +1,30 @@
+package Array;
+
+public class Main{
+	public static void main(String[] args){
+
+		// Declare an array
+		int[] arr = new int[5];
+
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+
+		// Intially elements of the array are 0
+		// How we can change the values of array
+
+		arr[0] = 5;
+		arr[1] = 23;
+		arr[3] = 50;
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+
+
+		
+	}
+}
