b2edda167fd096837dcb5799a328d3727fa87c16
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-07-24 09:00:43 +0530

    Frequency of String

--- String/FrequencyOfString.java
+++ String/FrequencyOfString.java
@@ -14,13 +14,16 @@ public class FrequencyOfString {
         int[] arr = new int[26];
 
         for (int i = 0; i < str.length(); i++) {
+            // Typecasting character to int which give us ascii value
             int x = str.charAt(i);
             int index = x - 97;
             arr[index]++;
         }
 
+
         for (int i = 0; i < 26; i++) {
             int ascii = i + 97;
+            // Typecasting Integer to Character back
             char ch = (char)ascii;
 
             if (arr[i] != 0) {
