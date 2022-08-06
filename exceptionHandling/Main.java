67139aea176eb68700cf2da2c6b64c075b828b5f
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-08-06 19:55:08 +0530

    Exception Handling with content

--- exceptionHandling/Main.java
+++ exceptionHandling/Main.java
@@ -57,3 +57,26 @@ public class Main {
 
 
 }
+
+
+
+
+//1. Try block
+// Java try block is used to enclose the code that might throw an exception. It must be used within the method. 
+
+
+// 2. CATCH block
+// Java catch block is used to handle the Exception by declaring the type of exception within the parameter. The declared exception must be the parent class exception ( i.e., Exception) or the generated exception type. However, the good approach is to declare the generated type of exception.
+
+
+// 3. FINALLY block
+// Java finally block is always executed whether an exception is handled or not. Therefore, it contains all the necessary statements that need to be printed regardless of the exception occurs or not.
+
+
+// 4.THROW Keyword
+// The Java throw keyword is used to throw an exception explicitly.
+// We specify the exception object which is to be thrown. The Exception has some message with it that provides the error description. These exceptions may be related to user inputs, server, etc.
+
+
+// 5. THROWS keyword
+// The Java throws keyword is used to declare an exception. It gives an information to the programmer that there may occur an exception. So, it is better for the programmer to provide the exception handling code so that the normal flow of the program can be maintained.
